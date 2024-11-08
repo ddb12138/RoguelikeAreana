f7835246d73f2e1adde8d58181fe8cdda91c384c
DDB <2323408377@qq.com>
2024-11-09 01:43:24 +0800

    人物描边、巫师低血隐身
    优化人物描边，能够有不同的光效
    巫师低血时会隐身并且增加移速

--- sences/game_object/wizard_enemy/wizard_enemy.gd
+++ sences/game_object/wizard_enemy/wizard_enemy.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 @onready var velocity_component = $VelocityComponent
 @onready var visuals = $Visuals
+@onready var health_component: HealthComponent = $HealthComponent
 
 
 var is_moving = false
@@ -24,3 +25,5 @@ func set_is_moving(moving: bool):
 
 func on_hit():
 	$HitRandomAudioPlayerComponent.play_random()
+	if health_component.get_health_percent() <= 0.5:
+		$AnimationPlayer.play("disappear")
