3b23be4a86a906116ab4e4b60d4cda12d75efb0c
DDB <2323408377@qq.com>
2024-11-11 00:30:47 +0800

    优化轮廓特效
    给愤怒巫师增加外观轮廓特效

--- sences/game_object/wizard_enemy/wizard_enemy.gd
+++ sences/game_object/wizard_enemy/wizard_enemy.gd
@@ -2,12 +2,16 @@ extends CharacterBody2D
 @onready var velocity_component = $VelocityComponent
 @onready var visuals = $Visuals
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var sprite_2d: Sprite2D = %Sprite2D
 
+var angry_state = preload("res://sences/game_object/wizard_enemy/angry_state.tres")
 
 var is_moving = false
 
 func _ready() -> void:
 	$HurtboxComponent.hit.connect(on_hit)
+	$HealthComponent.died.connect(on_die)
+	
 
 func _process(delta: float) -> void:
 	if is_moving:
@@ -27,3 +31,7 @@ func on_hit():
 	$HitRandomAudioPlayerComponent.play_random()
 	if health_component.get_health_percent() <= 0.5:
 		$AnimationPlayer.play("disappear")
+		sprite_2d.material = angry_state
+
+func on_die():
+	sprite_2d.modulate.a = 1
